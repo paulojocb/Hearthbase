688146991c5e9d5aa3600bbb27b20de4935a8110
Paulo José <paulojocb@gmail.com>
2018-11-30 16:06:54 -0300

    Adicionando sombra aos card de votação

--- Hearthbase/Extensions/CALayer.swift
+++ Hearthbase/Extensions/CALayer.swift
@@ -11,27 +11,27 @@ import UIKit
 
 extension CALayer {
     
-    func addRoundedCorner(radius: CGFloat) {
-        self.cornerRadius = radius
-        self.masksToBounds = true
-    }
-    
-    func addShadow(color: UIColor, alpha: Float = 0.5, x: CGFloat, y: CGFloat, blur: CGFloat = 0, spread: CGFloat) {
-        masksToBounds = false
-        shadowColor = color.cgColor
-        shadowOpacity = alpha
-        shadowOffset = CGSize(width: x, height: y)
-        shadowRadius = 4
-        
-        if spread == 0 {
-            shadowPath = nil
-        } else {
-            let dx = -spread
-            let rect = bounds.insetBy(dx: dx, dy: dx)
-            shadowPath = UIBezierPath(rect: rect).cgPath
-        }
-
-    }
+//    func addRoundedCorner(radius: CGFloat) {
+//        self.cornerRadius = radius
+//        self.masksToBounds = true
+//    }
+//    
+//    func addShadow(color: UIColor, alpha: Float = 0.5, x: CGFloat, y: CGFloat, blur: CGFloat = 0, spread: CGFloat) {
+//        masksToBounds = false
+//        shadowColor = color.cgColor
+//        shadowOpacity = alpha
+//        shadowOffset = CGSize(width: x, height: y)
+//        shadowRadius = 4
+//        
+//        if spread == 0 {
+//            shadowPath = nil
+//        } else {
+//            let dx = -spread
+//            let rect = bounds.insetBy(dx: dx, dy: dx)
+//            shadowPath = UIBezierPath(rect: rect).cgPath
+//        }
+//
+//    }
     
 
 }
